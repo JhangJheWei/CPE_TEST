a95fe1fbb4399b9d4464516f05dc99a4905a130c
JhangJheWei <sj340821@gmail.com>
2024-04-02 14:57:33 +0800

    exam24 was updated

--- 24/main.c
+++ 24/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -13,14 +14,20 @@ int main(int argc, char *argv[]) {
 		/*printf("distance:%d\n", distance);
 		printf("degree:%d\n", degree);
 		printf("name:%s\n", name);*/
-		if(name == "min"){
+		if(strcmp(name, "min") == 0){
 			degree/=60.0;
 		}
+		/*if(name == "min"){
+			degree/=60.0;
+		}*/
 		if(degree > 180.0){
 			degree = 360.0-degree;
 		}
+		//將角度轉換成徑度量(弧度) 
 		ang = degree*M_PI/180.0;
+		//計算弦長 
 		chord = 2.0*(distance+6440.0)*sin(ang/2.0);
+		//計算弧長 
 		arc = ang*(distance+6440.0);
 		printf("%.6lf %.6lf\n", arc, chord);
 	}
