fa0dc7b7898f05a8ec89203f6d81fc10230e6644
JhangJheWei <sj340821@gmail.com>
2024-04-04 14:34:56 +0800

    exam26 was finished

--- 26/main.c
+++ 26/main.c
@@ -10,7 +10,8 @@ int compare( const void *a, const void *b){
 
 int main(int argc, char *argv[]) {
 	char data[10000][32] = {0};
-	int i,ans,number,count=0;
+	int i,number,count=0;
+	float ans = 0;
 	
 	while(scanf("%d", &number) != EOF){
 		if(number != 0){
@@ -26,20 +27,16 @@ int main(int argc, char *argv[]) {
 			}
 			qsort(data, count, sizeof(data[0]), compare);
 			strcpy(data[count],"");
-			for(i=0;i<=count;i++){
-				printf("ans:%d\n", ans);
-				printf("count:%d\n", count);
-				printf("data[i]:%s\n", data[i]);
-				printf("data[i-1]:%s\n", data[i-1]);
+			for(i=1;i<=count;i++){
 				ans++;
 				if(strcmp(data[i-1],data[i])!=0){
-					printf("%s %f\n",data[i-1],(ans/count));
+					printf("%s %.4f\n",data[i-1],(ans/count)*100);
 					ans=0;
 				}
 			}
-			/*for(i=0;i<sizeof(10);i++){
-				printf("data:%s\n", data[i]);
-			}*/
+			if(number>1){
+				printf("\n");	
+			}
 			number--;
 		}
 	}
