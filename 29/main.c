734963422d6d23431d2347bd97d8e83fd7bebd80
JhangJheWei <sj340821@gmail.com>
2024-04-07 22:53:35 +0800

    exam29 was finished

--- 29/main.c
+++ 29/main.c
@@ -5,39 +5,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char data1[1000] = {0};
-	char data2[1000] = {0};
-	int i,j,k,count=0;
-	char el[29] = {0};
-	char test[] = "1abcdefghijklmnopqrstuvwxyz1";
-	
-	while(gets(data1) != NULL && gets(data2) != NULL){
-		k = 1;
-		for(i=0;i<sizeof(data1);i++){
-			if(data1[i] > 0){
-				for(j=0;j<sizeof(data2);j++){
-					if(data2[j] > 0){
-						if(data1[i] == data2[j]){
-							//printf("%c", data1[i]);
-							for(k=1;k<27;k++){
-								if(data1[i] == test[k]){
-									el[k] += 1;
-								}
-							}
-						}
-					}
-				}
-			}
+	char a[1001]={0},b[1001]={0};
+	while(scanf("%s %s",&a,&b)!=EOF){
+		int ca[26]={0},cb[26]={0},i,j;
+		for(i=0;a[i]!=0;i++){
+			ca[a[i]-'a']++;
 		}
-		for(k=1;k<27;k++){
-			for(;el[k]>0;el[k]--){
-				printf("%c", test[k]);
+		for(i=0;b[i]!=0;i++){
+			cb[b[i]-'a']++;
+		}
+		for(i=0;i<26;i++){
+			for(j=0;j<ca[i]&&j<cb[i];j++){
+				printf("%c",i+'a');
 			}
 		}
 		printf("\n");
-		memset(data1, 0 ,sizeof(data1));
-		memset(data2, 0 ,sizeof(data2));
-		memset(el, 0 ,sizeof(el));	
 	}
 	return 0;
 }
