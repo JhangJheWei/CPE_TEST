cb592f2d9cc21c720983f3eee2cf114e4442c13f
JhangJheWei <sj340821@gmail.com>
2024-04-09 23:29:21 +0800

    exam32 is begining

---
+++ 32/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char noises[] = {'c','d','e','f','g','a','b','C','D','E','F','G','A','B'};
+	int number = 0;
+	int data[11] = {0};
+	char input_data = "";
+	int i,j=0;
+	
+	while(scanf("%d", &number) != EOF){
+		while(number--){
+			gets(input_data);
+			for(i=0;i!='\0';i++){
+				for(j=0;j<sizeof(noises);j++){
+					if(input_data[i] == noises[j]){
+						
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
